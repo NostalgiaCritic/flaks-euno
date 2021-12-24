f519a684b3da209c5d0ff552072dc9ebd6446a97
NostalgiaCritic <88812101+NostalgiaCritic@users.noreply.github.com>
2021-12-24 09:42:46 +0900

    Delete pybo directory

--- pybo/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class ContactForm(FlaskForm):
-    name = StringField('성함', validators=[DataRequired("성함은 필수입력 항목입니다.")])
-    number = StringField('전화번호', validators=[DataRequired("전화번호는 필수입력 항목입니다.")])
--- pybo/models.py
+++
@@ -1,6 +0,0 @@
-from pybo import db
-
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    number = db.Column(db.String(20), nullable=False)
